8fbfe723d87c0a3acf0261f59cc73c8ee62ebdcf
wenxiaowei <wenxiaowei@sjtu.edu.cn>
2024-12-04 00:25:46 +0800

    fix: genY for complex

--- genY.m
+++ genY.m
@@ -1,6 +1,6 @@
 function Y = genY(H, X, snr, p)
     Y0 = H .* X;
-    powY = sum(Y0 .* Y0) / size(Y0, 2);
+    powY = sum(Y0 .* conj(Y0)) / size(Y0, 2);
     Y = awgn(H .* X, snr / p, powY, 'linear');
 end
 
