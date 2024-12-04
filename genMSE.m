3e0140d5812e334a6c205112b50e9da9c3706f35
wenxiaowei <wenxiaowei@sjtu.edu.cn>
2024-12-04 16:24:00 +0800

    fix: shorter genMSE implementation

--- genMSE.m
+++ genMSE.m
@@ -1,7 +1,3 @@
 function MSE = genMSE(Hhat, H, N)
-    MSE = 0;
-    for k = 1:N
-        MSE = MSE + abs(Hhat(k) - H(k)) ^ 2;
-    end
-    MSE = MSE / N;
+    MSE = sum(abs(Hhat - H) .^ 2) / N;
 end
